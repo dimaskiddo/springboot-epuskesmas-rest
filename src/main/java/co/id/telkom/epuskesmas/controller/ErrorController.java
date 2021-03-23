97ed0577f3fb66be436acdbda402abf3813ff6e0
Dimas Restu H <dimas.restu@student.upi.edu>
2021-03-24 02:19:31 +0700

    added default error controller and spring multipart config

---
+++ src/main/java/co/id/telkom/epuskesmas/controller/ErrorController.java
@@ -0,0 +1,59 @@
+package co.id.telkom.epuskesmas.controller;
+
+import co.id.telkom.epuskesmas.response.HandlerResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RequestMapping(produces={"application/json"})
+@RestController
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+
+    @Override
+    public String getErrorPath() {
+        return null;
+    }
+
+    @RequestMapping("/error")
+    public void errorHandler(HttpServletRequest request, HttpServletResponse response) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            Integer errorCode = Integer.valueOf(status.toString());
+
+            switch (errorCode) {
+                case 400:
+                    HandlerResponse.responseBadRequest(response, "");
+                    break;
+
+                case 401:
+                    HandlerResponse.responseUnauthorized(response, "");
+                    break;
+
+                case 403:
+                    HandlerResponse.responseForbidden(response, "");
+                    break;
+
+                case 404:
+                    HandlerResponse.responseNotFound(response, "");
+                    break;
+
+                case 500:
+                    HandlerResponse.responseInternalServerError(response, "");
+                    break;
+
+                case 502:
+                    HandlerResponse.responseBadGateway(response, "");
+                    break;
+
+                default:
+                    HandlerResponse.responseInternalServerError(response, "UNKNOWN ERROR");
+            }
+        } else {
+            HandlerResponse.responseSuccessOK(response, "NO ERROR");
+        }
+    }
+}
