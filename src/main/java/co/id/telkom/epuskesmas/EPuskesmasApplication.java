c5b239171a09ebc1eae2240046f5c4bc195f163a
Dimas Restu H <dimas.restu@student.upi.edu>
2021-03-23 10:43:19 +0700

    initial commit

---
+++ src/main/java/co/id/telkom/epuskesmas/EPuskesmasApplication.java
@@ -0,0 +1,13 @@
+package co.id.telkom.epuskesmas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPuskesmasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EPuskesmasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/id/telkom/epuskesmas/EPuskesmasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.id.telkom.epuskesmas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EPuskesmasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
