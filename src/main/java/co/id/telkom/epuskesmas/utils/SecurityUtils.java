5d039642e0110afb4935c6691d6da2cfd37b37de
Dimas Restu H <dimas.restu@student.upi.edu>
2021-03-25 00:25:56 +0700

    update utils Security constant

--- src/main/java/co/id/telkom/epuskesmas/utils/SecurityUtils.java
+++ src/main/java/co/id/telkom/epuskesmas/utils/SecurityUtils.java
@@ -2,8 +2,8 @@ package co.id.telkom.epuskesmas.utils;
 
 public class SecurityUtils {
 
-    public static final String SECRET = "GYF32mlnIXVuqAGuDgohzurOB9IBDg===";
-    public static final long EXPIRATION_TIME = 900_000; // 15 Minutes
+    public static final String SECRET = "GYF32mlnIXVuqAGuDgohzurOB9IBDg===";    // Shared Secret
+    public static final long EXPIRATION_TIME = 86400_000;                       // 24 Hours or 1 Day
     public static final String TOKEN_PREFIX = "Bearer";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_IN_URL = "/api/v1/login";
